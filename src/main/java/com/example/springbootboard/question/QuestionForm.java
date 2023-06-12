6036c63945867db2f66703e2aff66c6b90361420
an-jiohh <dkswlgh99@naver.com>
2023-06-13 00:50:18 +0900

    create validation form

---
+++ src/main/java/com/example/springbootboard/question/QuestionForm.java
@@ -0,0 +1,18 @@
+package com.example.springbootboard.question;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Setter
+@Getter
+public class QuestionForm {
+    @NotEmpty(message = "제목은 필수항목입니다.")
+    @Size(max=200)
+    private String subject;
+
+    @NotEmpty(message = "내용은 필수항목입니다.")
+    private String content;
+}
