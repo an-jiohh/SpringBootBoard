bd58ec38f31d19118328b11d7e472e65232aa38b
an-jiohh <dkswlgh99@naver.com>
2023-06-13 00:51:54 +0900

    create test for page search func

--- src/test/java/com/example/springbootboard/SpringBootBoardApplicationTests.java
+++ src/test/java/com/example/springbootboard/SpringBootBoardApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.springbootboard;
 
+import com.example.springbootboard.question.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootBoardApplicationTests {
-
+	@Autowired
+	private QuestionService questionService;
 	@Test
-	void contextLoads() {
+	void createQuestion() { //주의! 테스트데이터를 생성하는 함수
+		for (int i = 0; i < 300; i++) {
+			String subject = String.format("%d번 테스트데이터입니다.",i);
+			String content = "내용무";
+			questionService.create(subject,content);
+		}
 	}
 
 }
